cbeedf306572c2fc4fdc59b4e03157893b3c8bb8
viral sheladiya <sheladiyaviral2071998@gmail.com>
2023-01-01 19:09:06 +0530

    setting

--- src/Pages/Settings.js
+++ src/Pages/Settings.js
@@ -2,20 +2,22 @@ import { Box } from '@mui/material'
 import React from 'react'
 import SideNav from '../Components/SideNav'
 import Navbar from '../Components/Navbar'
+import List from './Settings/List'
 
 const Settings = () => {
   return (
     <>
+    <div className="bgcolor">
      <Navbar />
-    <Box height ={30}/>
+    <Box height ={70}/>
     <Box sx={{display:"flex"}}>
       <SideNav/>
-      <Box component="main" sx={{flexgrow: 1,p: 3}}>
-      setting
+      <Box component="main" sx={{flexgrow: 1,p: 3, width: "100%"}}>
+      <List />
       </Box>
   
     </Box>
-      
+    </div>
     </>
   )
 }
