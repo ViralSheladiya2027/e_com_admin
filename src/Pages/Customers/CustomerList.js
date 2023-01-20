328e3bdab9290b9ca51fc1813435bfedbda38cc8
viral sheladiya <sheladiyaviral2071998@gmail.com>
2023-01-20 19:04:59 +0530

    customer

--- src/Pages/Customers/CustomerList.js
+++ src/Pages/Customers/CustomerList.js
@@ -1,44 +1,101 @@
-import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
-import { Typography,Divider,Box,Paper,TableContainer} from "@mui/material"
+import * as React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { useState, useEffect } from "react";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {
+  Typography,
+  Divider,
+  Box,
+  Card,
+  CardContent,
+  TablePagination,
+  TableCell,
+  Stack,
+  TableRow,
+  Paper,
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+} from "@mui/material";
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "firstName", headerName: "First name", width: 130 },
+  { field: "lastName", headerName: "Last name", width: 130 },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
+    field: "age",
+    headerName: "Age",
+    type: "number",
     width: 90,
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
     sortable: false,
     width: 160,
     valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
   },
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
 ];
 
 export default function CustomerList() {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
   return (
+    // <>
+    //   <Card sx={{ minHeight: 84 + "vh" }}>
+    //     <CardContent>
+    //       <Typography
+    //         gutterBottom
+    //         variant="h5"
+    //         component="div"
+    //         sx={{ padding: "20px" }}
+    //       >
+    //         Customers
+    //       </Typography>
+
+    //       <Divider />
+    //       <Box height={15} />
+    //       <div style={{ height: 400, width: "100%" }}>
+    //         <DataGrid
+    //           rows={rows}
+    //           columns={columns}
+    //           pageSize={5}
+    //           rowsPerPageOptions={[5]}
+    //           checkboxSelection
+    //         />
+    //       </div>
+    //     </CardContent>
+    //   </Card>
+    // </>
+
     <>
-    <Paper sx={{ width: "100%",overflow: "hidden" }}>
+      {rows.length > 0 && (
+        <Paper sx={{ width: "100%", overflow: "hidden" }}>
           <TableContainer sx={{ maxHeight: 500 }}>
             <Typography
               gutterBottom
@@ -46,23 +103,115 @@ export default function CustomerList() {
               component="div"
               sx={{ padding: "20px" }}
             >
-             Customers
+              Customer
             </Typography>
 
             <Divider />
+
             <Box height={10} />
-            {/* <Box sx={{height:"100%"}}> */}
-            <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-      />
-            {/* </Box> */}
-     
-      </TableContainer>
-      </Paper>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="left" style={{ minWidth: "100px" }}>
+                    Full Name
+                  </TableCell>
+                  {/* <TableCell align="left" style={{ minWidth: "100px" }}>
+                  Image
+                </TableCell> */}
+                  <TableCell align="left" style={{ minWidth: "100px" }}>
+                    Mobile Number
+                  </TableCell>
+                  <TableCell align="left" style={{ minWidth: "100px" }}>
+                    Address
+                  </TableCell>
+                  <TableCell align="left" style={{ minWidth: "100px" }}>
+                    Email
+                  </TableCell>
+                  <TableCell align="left" style={{ minWidth: "100px" }}>
+                    Cart
+                  </TableCell>
+                  <TableCell align="left" style={{ minWidth: "100px" }}>
+                    Location
+                  </TableCell>
+                  <TableCell align="left" style={{ minWidth: "100px" }}>
+                    Action
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row) => {
+                    return (
+                      <TableRow
+                        hover
+                        key={row.id}
+                        role="checkbox"
+                        tabIndex={-1}
+                      >
+                        <TableCell align="left">{row.fullname}</TableCell>
+                        {/* <TableCell align="left">
+                        <img
+                          width="40px"
+                          height="35px"
+                          src={row.image}
+                          alt=""
+                        />
+                      </TableCell> */}
+                        <TableCell align="left">{row.mobilenumber}</TableCell>
+                        <TableCell align="left">{row.address}</TableCell>
+                        <TableCell align="left">{row.email}</TableCell>
+                        <TableCell align="left">{row.cart}</TableCell>
+                        <TableCell align="left">{row.location}</TableCell>
+
+                        <TableCell align="left">
+                          <Stack spacing={2} direction="row">
+                            <EditIcon
+                              style={{
+                                fontSize: "20px",
+                                color: "blue",
+                                cursor: "pointer",
+                              }}
+                              className="cursor-pointer"
+                              // onClick={() => {
+                              //   editData(
+                              //     row.id,
+                              //     row.name,
+                              //     row.image,
+                              //     row.price,
+                              //     row.unit
+                              //   );
+                              // }}
+                            />
+                            <DeleteIcon
+                              style={{
+                                fontSize: "20px",
+                                color: "darkred",
+                                cursor: "pointer",
+                              }}
+                              // onClick={() => {
+                              //   deleteUser(row.id);
+                              // }}
+                            />
+                          </Stack>
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Paper>
+      )}
     </>
   );
 }
